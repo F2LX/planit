66b47e4182d1f706d39db58c777e2eca976bb04c
Eneper <3nver.2510@gmail.com>
2024-04-01 15:33:14 +0700

    Vendor ads

--- resources/views/vendor/advertisement.blade.php
+++ resources/views/vendor/advertisement.blade.php
@@ -2,8 +2,25 @@
 
 @section('content')
 <div class="p-5">
-    <div class="w-100 d-flex">
-        <h2>Halo</h2>
+    <div class="w-100 d-flex flex-direction-column">
+        <h2>Post New ads</h2>
+        <form action="">
+
+            <div class="d-flex">
+                <p>Title:</p>
+                <textarea name="" id="" cols="30" rows="10"></textarea>
+            </div>
+    
+            <div class="d-flex">
+                <p>Description:</p>
+                <textarea name="" id="" cols="30" rows="10"></textarea>
+            </div>
+    
+            <div class="d-flex align-center">
+                <p>Upload Images:</p>
+                <input type="file" name="" id="">
+            </div>
+        </form>
     </div>
 </div>
 

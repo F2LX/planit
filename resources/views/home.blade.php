b9bbe2c79bf78ff0dcc3fa80712b1ad94e202de3
F2LX <felixfebryan16@gmail.com>
2024-04-17 12:04:25 +0700

    Update homepage (Naomi)

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -7,9 +7,9 @@
     </div>
     <div class="utama">
 
-        <div class="section-search">
-            <h1 class="big-text-plan">PLAN</h1>
-            <h2 id="main" class="small-text-event">YOUR EVENT NOW</h2>
+        <div class="section-search-naomi">
+            <h1 class="big-text-plan-naomi">PLAN</h1>
+            <h2 id="main" class="small-text-event-naomi">YOUR EVENT NOW</h2>
             <form class="searchbar d-flex justify-content-center " action="" method="get">
                 @csrf
                 <input type="text" name="search" id="search" placeholder="Event organizer, birthday party, wedding organizer">
@@ -19,12 +19,12 @@
             </form>
         </div>
 
-        <div class="section-top-vendor">
+        <div class="section-top-vendor-naomi">
             <img class="vendor-top-img" src="{{ asset("img/homecouples.png") }}" alt="">
             <h2 class="bigtitle">Plan Your Event Now!</h2>
 
             <div class="classdiv">
-                <div class="card">
+                <div class="card-naomi">
                     <img class="image" src="https://t.ly/t8yhZ" alt="">
                     <h3 class="title">Taylor Series Vendor</h3>
                     <p class="price">Start from $500</p>
@@ -47,7 +47,7 @@
                     </div>
                 </div>
 
-                <div class="card">
+                <div class="card-naomi">
                     <img class="image" src="https://t.ly/WaJdC" alt="">
                     <h3 class="title">Regression Event</h3>
                     <p class="price">Start from $750</p>
@@ -70,7 +70,7 @@
                     </div>
                 </div>
 
-                <div class="card">
+                <div class="card-naomi">
                     <img class="image" src="https://t.ly/l2603" alt="">
                     <h3 class="title">For Happiness</h3>
                     <p class="price">Start from $1500</p>
@@ -93,7 +93,7 @@
                     </div>
                 </div>
 
-                <div class="card">
+                <div class="card-naomi">
                     <img class="image" src="https://images.unsplash.com/photo-1511795409834-ef04bbd61622?q=80&w=2338&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" alt="">
                     <h3 class="title">Flower Smile</h3>
                     <p class="price">Start from $600</p>
@@ -116,7 +116,7 @@
                     </div>
                 </div>
                 
-                <div class="card">
+                <div class="card-naomi">
                     <img class="image" src="https://images.unsplash.com/photo-1524824267900-2fa9cbf7a506?q=80&w=2340&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" alt="">
                     <h3 class="title">Aligning 19</h3>
                     <p class="price">Start from $1200</p>
@@ -139,7 +139,7 @@
                     </div>
                 </div>
 
-                <div class="card">
+                <div class="card-naomi">
                     <img class="image" src="https://images.unsplash.com/photo-1560332218-4d266ab9793a?q=80&w=2334&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" alt="">
                     <h3 class="title">Sweet, Sugar</h3>
                     <p class="price">Start from $1000</p>
@@ -162,7 +162,7 @@
                     </div>
                 </div>
 
-                <div class="card">
+                <div class="card-naomi">
                     <img class="image" src="https://images.unsplash.com/photo-1523301343968-6a6ebf63c672?q=80&w=2338&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" alt="">
                     <h3 class="title">Run Run Away</h3>
                     <p class="price">Start from $870</p>
@@ -185,7 +185,7 @@
                     </div>
                 </div>
               
-                <div class="card">
+                <div class="card-naomi">
                     <img class="image" src="https://images.unsplash.com/photo-1562967005-a3c85514d3e9?q=80&w=2340&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" alt="">
                     <h3 class="title">Save Memories</h3>
                     <p class="price">Start from $770</p>
@@ -210,10 +210,10 @@
             </div>
         </div>
 
-        <div class="section-new ">
-            {{-- <h2 class="judul">Testimonials</h2>
-            <h1 class="subjudul">From Our Customer</h1> --}}
+        <div class="section-new">
             <div class="card-new">
+                <h1 class="judul">Testimonial</h1>
+                <h2 class="subjudul">From Our Customer</h2>
                 <div class="card2">
                     <div class="img-wrapper-home">
                         <img class="image-person" src="https://cdn1-production-images-kly.akamaized.net/bINtHqzUW95VVjtjDCvkh1SQOjo=/1200x900/smart/filters:quality(75):strip_icc():format(webp)/kly-media-production/medias/2922109/original/010429200_1569423175-Nagita_Slavina_foto_by_Bambang_E_Ros_8.jpg" alt="">
@@ -227,6 +227,8 @@
             </div>
 
             <div class="card-new">
+                <h1 class="judul">Testimonial</h1>
+                <h2 class="subjudul">From Our Customer</h2>
                 <div class="card2">
                     <div class="img-wrapper-home">
                         <img class="image-person" src="https://asset-a.grid.id/crop/0x0:0x0/x/photo/2021/02/08/20210208_140640jpg-20210208013446.jpg" alt="">
@@ -240,6 +242,8 @@
             </div>
 
          <div class="card-new">
+            <h1 class="judul">Testimonial</h1>
+            <h2 class="subjudul">From Our Customer</h2>
                 <div class="card2">
                     <div class="img-wrapper-home">
                         <img class="image-person" src="https://shopee.co.id/inspirasi-shopee/wp-content/uploads/2023/08/Raffi-Ahmad-kekayaan-6.jpg" alt="">
@@ -254,7 +258,10 @@
 
         </div>
 
-        <div>
+        <div class="copyright">
+
+            {{-- yang ada gambarnya itu loh aku gabisa... yang kek nyatu ke kotaknya terus gambarnya dempetan HELP --}}
+
 
         </div>
     </div>
